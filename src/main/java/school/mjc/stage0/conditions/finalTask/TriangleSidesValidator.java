ccc13b5b6e5e32522204a799012fa888ee2e9a0d
Bago0 <jgamadzebagrati@gmail.com>
2023-02-07 23:50:25 +0400

    conditions_taskFinal(update1)

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,10 +2,32 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if((firstSide * firstSide + secondSide * secondSide) >= (thirdSide*thirdSide)){
-            System.out.println("this is a valid triangle");
+        double biggest;
+        if(firstSide > secondSide && firstSide > thirdSide){
+            biggest = firstSide;
+        }else if(secondSide > firstSide && secondSide > thirdSide){
+            biggest = secondSide;
         }else{
-            System.out.println("it's not a triangle");
+            biggest = thirdSide;
+        }
+        if(biggest == firstSide) {
+            if (secondSide+thirdSide > firstSide) {
+                System.out.println("this is a valid triangle");
+            } else {
+                System.out.println("it's not a triangle");
+            }
+        } else if (biggest == secondSide) {
+            if (firstSide+thirdSide > secondSide) {
+                System.out.println("this is a valid triangle");
+            } else {
+                System.out.println("it's not a triangle");
+            }
+        } else {
+            if (secondSide+firstSide > thirdSide) {
+                System.out.println("this is a valid triangle");
+            } else {
+                System.out.println("it's not a triangle");
+            }
         }
     }
 }
